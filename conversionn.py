60d13c9f8d387acc6e780055e250717a9c4c07ef
RafaRomero8 <RafaRomerox@gmail.com>
2020-08-11 20:40:06 -0500

    Utilzando funciones y condicional if

---
+++ conversionn.py
@@ -0,0 +1,73 @@
+#conversión de pesos mexicanos a dolaes
+
+"""pesos = input("¿Cuantos pesos tienes?: ")
+pesos = float(pesos)
+valor_dollar = 22.38
+dollars = pesos / valor_dollar
+dollars = round(dollars,2)
+dollars = str(dollars)
+print("Tienes $" + dollars + " dollars ")
+
+#conversión de dolares a pesos mexicanos
+
+dolar = input("¿Cuantos dollars tienes?: ")
+dolar = float(dolar)
+valor_dollar = 22.38
+pesos = dolar * valor_dollar
+pesos = round(pesos,2)
+pesos = str(pesos)
+print("Tienes $" + pesos + " pesos mexicanos ")
+
+edad = int(input("Escribe tu edad"))
+if edad > 17:
+    print("eres mayor de edad")
+else:
+    print("eres menor de edad")"""
+
+def conversor(tipo_pesos, valor_dollar):
+    pesos = input("¿Cuantos pesos " + tipo_pesos + " tienes?: ")
+    pesos = float(pesos)
+    dollars = pesos / valor_dollar
+    dollars = round(dollars, 2)
+    dollars = str(dollars)
+    print("Tienes $" + dollars + " dollars ")
+
+
+
+
+
+menu = """
+Bienvenido al conversor de monedas 💲
+
+1- Pesos mexicanos
+2- Pesos argentino
+3- Pesos colombiano
+
+Elige una opción:  """
+
+opcion = int (input(menu))
+
+if opcion == 1:
+    conversor("mexicanso",24)
+
+elif opcion == 2:
+    conversor("argentinos", 65)
+
+elif opcion == 3:
+    conversor("colombianos",3875)
+
+else:
+    print("Ingresa una opcion valida")
+
+
+
+
+"""numero = int(input("escribe un número: "))
+
+if numero > 5:
+    print("Es mayor a 5")
+elif numero == 5:
+    print("Es igual a 5")
+else:
+    print("Es menor a 5")"""
+
---
+++ funciones_platz.py
@@ -0,0 +1,30 @@
+
+"""def imprimir_mensaje():
+    print('Mensaje especial: ')
+    print('Estoy aprendiendo a usar funciones!')
+
+imprimir_mensaje() """
+''' conversacion(mensaje):
+    print('Hola')
+    print('como estas')
+    print(mensaje)
+    print('bye')
+
+
+opcion = int (input("eligie una opción (1,2,3)"))
+if opcion == 1:
+   conversacion('elijiste la opcion 1')
+elif opcion == 2:
+    conversacion('elijiste la opcion 2')
+elif opcion == 3:
+    conversacion('elijiste la opcion 3')
+else:
+    print('esciribe la opcion correcta')'''
+
+def suma(a, b):
+   print('la suma de doss numeros')
+   resultado = a + b
+   return resultado
+
+sumatoria = suma(2, 3)
+print(sumatoria)
