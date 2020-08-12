629c07637f0b41b5802d824df1beb2ba26f94dba
RafaRomero8 <RafaRomerox@gmail.com>
2020-08-11 19:46:27 -0500

    Mi primer curso phyton

---
+++ cadenas_platz.py
@@ -0,0 +1,27 @@
+"""nombre = input("¿cuál es tu nombre?")
+print(nombre.upper())
+
+nombre2 = input("¿cuál es tu nombre?")
+print(nombre2.capitalize())
+
+nombre3 = input("¿cuál es tu nombre?")
+print(nombre3.strip())
+
+nombre3 = input("¿cuál es tu nombre?")
+print(nombre3.lower()) """
+
+"""  nombre4 = input("¿cuál es tu nombre?")
+print(nombre4.replace('a','o'))
+
+
+nombre3 = input("¿cuál es tu nombre?")
+print(nombre3[2])
+
+nombre3 = input("¿cuál es tu nombre?")
+print(len(nombre3))       """
+
+nombre="Rafael"
+
+print(nombre[0:3])
+print(nombre[0:5:2])
+print(nombre[::-1])
