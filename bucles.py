160cf9e883c17f1060ff9c2a7631de10e00a4e72
RafaRomero8 <RafaRomerox@gmail.com>
2020-08-13 15:23:02 -0500

    bucle con while

--- bucles.py
+++ bucles.py
@@ -1,5 +1,18 @@
-contador = 2
-print('2 elevado a '+ str(contador) + ' es igual a: ' + str(2**contador))
+"""contador = 2
+print('2 elevado a '+ str(contador) + ' es igual a: ' + str(2**contador))"""
 
 
-dif
+def run():
+    LIMITE = 1000  #si ponemos una variable en mayuscula se convierte en una constante
+
+    contador = 0
+    potencia_2 = 2 **contador #dos elevado a contador
+    while potencia_2 < LIMITE:
+        print('2 elevado a ' + str(contador) + ' es igual a: ' + str(potencia_2))
+        contador = contador + 1 #
+        potencia_2 = 2**contador
+
+
+
+if __name__ == '__main__':
+    run()
