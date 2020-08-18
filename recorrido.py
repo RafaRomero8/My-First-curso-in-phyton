d8ec95a968085195082f21b44465dc13c1917b04
RafaRomero8 <RafaRomerox@gmail.com>
2020-08-18 15:36:04 -0500

    recorrido for

---
+++ recorrido.py
@@ -0,0 +1,8 @@
+def run():
+    nombre = input('escribe tu nombre')
+    for letra in nombre:
+        print(letra)  # para cada letra en nonmbre va a imprimir la letra en el ciclo
+
+
+if __name__ == '__main__':
+    run()
