86e8fbc7c9550121d6081646af8359c19126e9fa
RafaRomero8 <RafaRomerox@gmail.com>
2020-08-18 18:27:35 -0500

    ciclo con break y continue

--- bucles.py
+++ bucles.py
@@ -3,12 +3,7 @@ print('2 elevado a '+ str(contador) + ' es igual a: ' + str(2**contador))"""
 
 
 def run():
-    for i in range(10):
-        print(11 * i)
-
-   
-
-""" LIMITE = 1000  #si ponemos una variable en mayuscula se convierte en una constante
+   """ LIMITE = 1000  #si ponemos una variable en mayuscula se convierte en una constante
 
     contador = 0
     potencia_2 = 2 **contador #dos elevado a contador
@@ -20,10 +15,7 @@ def run():
    for contador in range(1, 1001): #para la variable contador en  el rango que va de 1 a 1001 range(que va desde 0 al 999)
        print(contador) #se va a imprimir contador del 1 a 1001"""
 
-
-
-
-
+   for i in range(10):print(11 * i)
 
 if __name__ == '__main__':
     run()
--- recorrido.py
+++ recorrido.py
@@ -1,8 +1,25 @@
-def run():
-    nombre = input('escribe tu nombre')
-    for letra in nombre:
-        print(letra)  # para cada letra en nonmbre va a imprimir la letra en el ciclo
 
+def runi():
+    for i in range(1000):
+        print(i)
+        if i == 5678:
+
+
+
+   # frase = input('escribe una frase')
+    #for palabra in frase:
+     #   print(palabra.strip())
+    #nombre = input('escribe tu nombre')
+    #for letra in nombre:
+     #   print(letra[::-1])  # para cada letra en nonmbre va a imprimir la letra en el ciclo
+
+
+# for contador in range(1000): #se ejecuta un ciclo pero cada vez que se encuentre un numero impar se salta el numero y no ko imprime
+ #         if contador % 2 != 0:
+  #            continue #apartir de continue lo que esta despues  no se va a ejecutar
+   #       print(contador)
+
+#ejemplo de ciclo con break y continue
 
 if __name__ == '__main__':
-    run()
+    runi()
