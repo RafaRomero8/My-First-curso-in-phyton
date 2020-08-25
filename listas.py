7bf30494a51965bcb840171cada951f73aa599fb
RafaRomero8 <RafaRomerox@gmail.com>
2020-08-24 20:22:13 -0500

    listas 2

--- listas.py
+++ listas.py
@@ -1,4 +1,4 @@
-numeros = [3,1,5,True,5,12,67]
+"""numeros = [3,1,5,True,5,12,67]
 #una lista contiene varios elementos,datos de un mismo tipo o diferente
 print(numeros)
 
@@ -13,4 +13,14 @@ objetos.pop(1)
 print(objetos)
 objeto = [3,True,'hola',88,34]
 for elemento in objeto:
-    print(elemento)
+    print(elemento)"""
+numero_1 = [1,2,3,4]
+numero_2 = [5,6,7]
+print(numero_1 + numero_2)
+
+#tuplas no se pueden borrar con la funco pp(),son estaticos a diferrencia de lsitas quee son dinamicas
+#se ejecutan mas rapidos con el for haciendo el recorrido
+#no se pueden agrega elemntos ni quitar,no se pueden modificar
+mi_tupla = (1,2,3,4,5)
+for numero in mi_tupla:
+    print(numero)
