edb0fd8c343d37d08db00a96c215d231aaddf8f6
RafaRomero8 <RafaRomerox@gmail.com>
2020-08-27 17:11:27 -0500

    aproximacion de solucones

--- exhaustiva.py
+++ exhaustiva.py
@@ -1,5 +1,18 @@
 def run():
-    objetivo = int(input('escoge un entero'))
+    objetivo = int(input('escoge un numero'))
+    epsilon = 0.0001 #presicion que queremos
+    paso = epsilon**2 #mas pequeño que epsilon
+    respuesta = 0.0 #variable para guardar la respuesta
+
+    while abs(respuesta**2 - objetivo) >= epsilon and respuesta <= objetivo:  # conforme el valor absoluto sde acerca a epsilon se va a llegar mas cerca
+        #abs()regresa el valor absoluto
+        print(abs(respuesta**2 - objetivo),respuesta)
+        respuesta += paso #respuesta aumenta el paso en cada iteracion sube
+    if abs(respuesta**2 - objetivo) >= epsilon:
+        print(f'no se encontro la raiz cuadrada de {objetivo}')
+    else:
+        print(f'la raiz cuadrada de {objetivo} es {respuesta}')
+"""objetivo = int(input('escoge un entero'))
     respuesta = 0
   #determina si un numero tiene la raiz cuadrada exacta
     while respuesta**2 < objetivo:
@@ -8,7 +21,7 @@ def run():
     if respuesta**2 == objetivo:
         print(f'la raiz cuadrada de {objetivo} es {respuesta}')
     else:
-        print(f'{objetivo} no tiene una raiz cuadrada exacta')
+        print(f'{objetivo} no tiene una raiz cuadrada exacta')"""
 
 
 
