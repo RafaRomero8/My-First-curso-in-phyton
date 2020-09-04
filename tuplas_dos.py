ae8651a13db512aec5d9fded1c855d2debdce9ef
RafaRomero8 <RafaRomerox@gmail.com>
2020-09-03 19:58:03 -0500

    tuplas desempaquetar

---
+++ tuplas_dos.py
@@ -0,0 +1,33 @@
+def run():
+    def coordenadas():
+        return (5,4)
+
+
+    coordenada = coordenadas()
+    coordenada
+    print(coordenada)
+
+    x,y = coordenadas() #desempaquetada
+    print(x,y)
+
+    my_tuple = (8,)
+    print(my_tuple)
+
+
+#el contenido de las tuplas  pueden ser de tipo int,float y string
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+if __name__ == '__main__':
+        run()
