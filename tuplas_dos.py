9c327c2222b1686adf84a2c3c8366a3984a1ef18
RafaRomero8 <RafaRomerox@gmail.com>
2020-09-03 20:11:12 -0500

    rangos

--- tuplas_dos.py
+++ tuplas_dos.py
@@ -13,7 +13,21 @@ def run():
     my_tuple = (8,)
     print(my_tuple)
 
+    # range(comienzo,fin,pasos)
 
+    my_range = range(1, 5)
+    my_ranges = range(0, 7, 2)
+
+    print(type(my_range))#tipo de esta variable
+    for i in my_range:
+        print(i)  #es no inclusivo no esta incluyendo el final
+
+    for h in my_ranges:
+        print(h)
+
+    for k in range(0,21,2):
+        print('------------')
+        print(k + 1)
 #el contenido de las tuplas  pueden ser de tipo int,float y string
 
 
