28b55f679a1eaffe090cf3d790b71d4e2dc50a23
RafaRomero8 <RafaRomerox@gmail.com>
2020-09-14 21:19:11 -0500

    asserts

--- excepciones.py
+++ excepciones.py
@@ -8,3 +8,7 @@ lista = list(range(10))
 divisor = 0
 
 print(divide_elemenos_de_lista(lista,divisor))
+"""usa asserts hace parte de la programación defensiva, debes usarlos siempre que sepas que algodebe cumplirse,
+ se usa mucho con parametros de las funciones.por ejemplo una función que divide dos números 
+ a y b uno sabe que b o el divisor no puede ser 
+ cero entonces uno puede usar un assert"""
